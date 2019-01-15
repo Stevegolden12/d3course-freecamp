42ae498a7fa01f0b7f1a492a5c9fe71b523e0559
Steve Golden <sgolden2525@outlook.com>
2019-01-15 11:12:45 -0600

    Learning Axes

---
+++ Axes/index.js
@@ -0,0 +1,32 @@
+//JavaScript
+var data = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500, svgHeight = 300, barPadding = 5;
+var barWidth = (svgWidth / data.length);
+
+var svg = d3.select('svg')
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+
+/* Scales */
+var xScale = d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([0, svgWidth]);
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(data)])
+  .range([svgHeight, 0])
+
+var x_axis = d3.axisBottom().scale(xScale);
+
+var y_axis = d3.axisLeft().scale(yScale);
+
+svg.append("g")
+  .attr("transform", "translate(50,10)")
+  .call(y_axis);
+
+var xAxisTranslate = svgHeight - 20;
+
+svg.append("g")
+  .attr("transform", "translate(50, " + xAxisTranslate + ")")
+  .call(x_axis);
